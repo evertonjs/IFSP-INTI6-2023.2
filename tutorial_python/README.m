76f3ea0058d26e2b30d661115c7cc383d3d3afc4
Everton Silva <evertonjsilva31@gmail.com>
2023-08-26 10:22:09 -0300

    Create README.m

---
+++ tutorial_python/README.m
@@ -0,0 +1,32 @@
+# Tutorial Python para Iniciantes
+
+Bem-vindo ao Tutorial Python para Iniciantes! Este repositório contém recursos abrangentes e exemplos práticos para ajudar você a começar com a programação em Python, independentemente do seu nível de experiência.
+
+## O que você aprenderá
+
+- Instalação e configuração do ambiente Python.
+- Conceitos fundamentais da linguagem Python.
+- Como escrever e executar seu primeiro programa Python.
+- Estruturas de controle de fluxo, como condicionais e loops.
+- Trabalhar com listas, dicionários e outras estruturas de dados.
+- Funções e modularização de código.
+- Noções básicas de manipulação de arquivos.
+- Boas práticas de programação e estilo de código.
+
+## Como usar este tutorial
+
+Este tutorial é organizado em uma série de pastas, cada uma cobrindo um tópico específico. Cada pasta contém código de exemplo, explicações detalhadas e exercícios para reforçar o aprendizado. Sinta-se à vontade para explorar os tópicos na ordem que preferir.
+
+## Requisitos
+
+Antes de começar, você precisará:
+
+- Ter o Python instalado em seu sistema.
+- Um editor de código de sua escolha (recomendamos Visual Studio Code ou PyCharm).
+- Vontade de aprender e experimentar!
+
+## Contribuições
+
+Se você encontrar erros, tiver sugestões de melhoria ou quiser adicionar mais conteúdo ao tutorial, sinta-se à vontade para criar problemas (issues) ou enviar solicitações de pull (pull requests). Este tutorial é um recurso em constante evolução para a comunidade Python, e agradecemos sua contribuição!
+
+Vamos começar sua jornada de programação Python. Divirta-se aprendendo!
